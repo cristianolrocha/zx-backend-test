46e186e3e7f9cf5b15a4bac8f6cc8e61e09947bd
crocha@acessocard.com.br <crocha@ACDESENV36>
2019-03-28 19:02:42 -0300

    Adicioandas as secrets de BD

--- src/pdv/Program.cs
+++ src/pdv/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace pdv
 {
@@ -19,6 +13,13 @@ namespace pdv
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(cfg =>
+                {
+                    cfg.AddDockerSecrets(dcfg =>
+                    {
+                        dcfg.Optional = true;
+                    });
+                })
                 .UseStartup<Startup>();
     }
 }
--- src/pdv/Repositories/Base/MongoDBConfig.cs
+++ src/pdv/Repositories/Base/MongoDBConfig.cs
@@ -4,7 +4,7 @@
     {
         public string Database { get; set; }
         public string Host { get; set; }
-        public int Port { get; set; }
+        public int Port { get; set; } = 27017;
         public string User { get; set; }
         public string Password { get; set; }
         public string ConnectionString
